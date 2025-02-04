5bc25b289021430e5a4da39e306ee8607f72babf
Artur Baldo Ramos <arturbaldoramos@gmail.com.com>
2025-02-03 22:03:54 -0300

    add configuration management with viper and environment variables

--- main.go
+++ main.go
@@ -6,24 +6,24 @@ import (
 
 	"github.com/arturbaldoramos/simple-bank/api"
 	db "github.com/arturbaldoramos/simple-bank/db/sqlc"
+	"github.com/arturbaldoramos/simple-bank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(":8080")
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server: ", err)
 	}
