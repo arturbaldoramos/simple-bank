c7905b40e1eaace47e4bf0727062ec03d8b937a5
Artur Baldo Ramos <arturbaldoramos@gmail.com>
2025-03-20 00:06:09 -0300

    implemented test server and variables

--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,25 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
+	db "github.com/arturbaldoramos/simple-bank/db/sqlc"
+	"github.com/arturbaldoramos/simple-bank/util"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymetricKey:   util.RandomString(32),
+		AcessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
